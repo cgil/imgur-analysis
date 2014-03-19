5c7464ec9dd84a022710b673dca0ea610be560be
Carlos Gil <cgil1210@gmail.com>
2014-03-19 16:14:01 -0400

    impemented api for mongo

--- api.py
+++ api.py
@@ -1,30 +1,71 @@
 from flask import Flask
 from pymongo import MongoClient
+from bson.json_util import dumps
 
 app = Flask(__name__)
 
 client = MongoClient()
 db = client.imgur
 
-@app.route("/hour/<which>")
+@app.route("/hour/<which>/")
 def hour(which=''):
-	return db[hour+which].find()
+	return dumps(db['hour'+str(which)].find())
+
+@app.route("/hour/<which>/<shotType>")
+def hourType(which='', shotType=None):
+	if shotType == None:
+		query = {}
+	else:
+		query = { 'shotType' : shotType }
+	return dumps(db['hour'+str(which)].find(query))
 
 @app.route("/weekday/<which>")
 def weekday(which=''):
-	return db[weekday+which].find()
+	return db['weekday'+str(which)].find()
+
+@app.route("/weekday/<which>/<shotType>")
+def weekdayType(which='', shotType=None):
+	if shotType == None:
+		query = {}
+	else:
+		query = { 'shotType' : shotType }
+	return dumps(db['weekday'+str(which)].find(query))
 
 @app.route("/month/<which>")
 def month(which=''):
-	return db[month+which].find()
+	return db['month'+str(which)].find()
+
+@app.route("/month/<which>/<shotType>")
+def monthType(which='', shotType=None):
+	if shotType == None:
+		query = {}
+	else:
+		query = { 'shotType' : shotType }
+	return dumps(db['month'+str(which)].find(query))
 
 @app.route("/year/<which>")
 def year(which=''):
-	return db[year+which].find()
+	return db['year'+str(which)].find()
+
+@app.route("/year/<which>/<shotType>")
+def yearType(which='', shotType=None):
+	if shotType == None:
+		query = {}
+	else:
+		query = { 'shotType' : shotType }
+	return dumps(db['year'+str(which)].find(query))
 
 @app.route("/delta/<which>")
 def delta(which=''):
-	return db[delta+which].find()
+	return db['delta'+str(which)].find()
+
+@app.route("/delta/<which>/<shotType>")
+def deltaType(which='', shotType=None):
+	if shotType == None:
+		query = {}
+	else:
+		query = { 'shotType' : shotType }
+	return dumps(db['delta'+str(which)].find(query))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
